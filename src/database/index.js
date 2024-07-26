aae65768f8b36d4872de409b1b96ee11a03bc877
Glauton Osório <glautonleao@gmail.com>
2024-07-26 05:40:10 -0300

    feat: criando os modelos de users e address

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,22 @@
 const Sequelize = require("sequelize");
 const configDB = require("./config/database");
+
+const User = require("./models/users.model");
 const Permission = require("./models/permissions.model");
 const TypesPermission = require("./models/typesPermission.model");
 const UsersTypes = require("./models/usersTypes.model");
+const Address = require("./models/address.model");
 
-const models = { TypesPermission, Permission, UsersTypes };
+const models = { User, Permission, TypesPermission, UsersTypes, Address };
 
 const connection = new Sequelize(configDB);
 
 Object.values(models).forEach((model) => {
-  model.init(connection);
+  if (model.init) model.init(connection);
 });
 
 Object.values(models).forEach((model) => {
-  if (model.associate) {
-    model.associate(models);
-  }
+  if (model.associate) model.associate(models);
 });
 
 module.exports = connection;
