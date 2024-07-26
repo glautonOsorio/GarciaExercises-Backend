04161686fc73d4f8c7b8ddf103daab481ff4a95a
Glauton Osório <glautonleao@gmail.com>
2024-07-26 04:13:08 -0300

    feat: modelos de permission, typePermissions e usersTypes

---
+++ src/database/models/permissions.model.js
@@ -0,0 +1,30 @@
+// models/Permissions.model.js
+const { Model, DataTypes } = require("sequelize");
+
+class Permission extends Model {
+  static associate(models) {
+    this.belongsToMany(models.UsersTypes, {
+      through: models.TypesPermission,
+      foreignKey: "permissionId",
+      as: "userTypes",
+    });
+  }
+
+  static init(sequelize) {
+    super.init(
+      {
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "Permission",
+        tableName: "permissions",
+      }
+    );
+  }
+}
+
+module.exports = Permission;
---
+++ src/database/models/typesPermission.model.js
@@ -0,0 +1,37 @@
+// models/TypesPermission.model.js
+const { Model, DataTypes } = require("sequelize");
+
+class TypesPermission extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        typeId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: "usersTypes",
+            key: "id",
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        },
+        permissionId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: "permissions",
+            key: "id",
+          },
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        },
+      },
+      {
+        sequelize,
+        modelName: "TypesPermission",
+        tableName: "typesPermissions",
+        timestamps: true,
+      }
+    );
+  }
+}
+
+module.exports = TypesPermission;
