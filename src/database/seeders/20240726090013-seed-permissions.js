e71cbed7eb082843adb011190d2cd61c0e3df446
Glauton Osório <glautonleao@gmail.com>
2024-08-05 07:51:59 -0300

    fix: atualizando as permissões dos tipos de usuários

--- src/database/seeders/20240726090013-seed-permissions.js
+++ src/database/seeders/20240726090013-seed-permissions.js
@@ -3,51 +3,226 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("permissions", [
-      {
-        name: "create_record",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "read_record",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "update_record",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "delete_record",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "create_test",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "read_test",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "update_test",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "delete_test",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    try {
+      const [usersTypes] = await queryInterface.sequelize.query(
+        `SELECT id, name FROM "usersTypes";`
+      );
+
+      const [permissions] = await queryInterface.sequelize.query(
+        `SELECT id, name FROM "permissions";`
+      );
+
+      const userTypeIds = usersTypes.reduce((acc, type) => {
+        acc[type.name] = type.id;
+        return acc;
+      }, {});
+
+      const permissionIds = permissions.reduce((acc, perm) => {
+        acc[perm.name] = perm.id;
+        return acc;
+      }, {});
+
+      const typesPermissions = [
+        // Admin Permissions
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.create_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.read_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.update_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.delete_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.create_admin,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.read_admin,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.update_admin,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.delete_admin,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.create_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.read_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.read_local_map,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.update_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.admin,
+          permissionId: permissionIds.delete_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+        // Basic and Premium Permissions
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.create_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.read_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.update_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.delete_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.create_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.read_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.read_local_map,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.update_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.basico,
+          permissionId: permissionIds.delete_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.create_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.read_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.update_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.delete_user,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.create_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.read_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.read_local_map,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.update_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          typeId: userTypeIds.premium,
+          permissionId: permissionIds.delete_local,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      await queryInterface.bulkInsert("typesPermissions", typesPermissions, {});
+    } catch (error) {
+      console.error("Error in seeder:", error);
+      throw error;
+    }
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("permissions", null, {});
+    await queryInterface.bulkDelete("typesPermissions", null, {});
   },
 };
